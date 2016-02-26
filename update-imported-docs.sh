aed351a3340ef04bbcde5f43f1b2afc97405f032
John Mulhausen <johndylan@johndylan-macbookpro2.roam.corp.google.com>
2016-02-26 04:31:34 -0800

    Ran update-imported-docs.sh

--- update-imported-docs.sh
+++ update-imported-docs.sh
@@ -1,22 +1,24 @@
-#git clone https://github.com/kubernetes/kubernetes.git k8s
-#cd k8s
-#git checkout gh-pages
-#cd ..
-
-
+git clone https://github.com/kubernetes/kubernetes.git k8s
+cd k8s
+git checkout gh-pages
+cd ..
 
 while read line || [[ -n ${line} ]]; do
-mystring="line"
-
-IFS=',' read -a myarray <<< "$mystring"
-
-echo "IP: ${myarray[0]}"
-echo "STATUS: ${myarray[3]}"
-  CLEARPATH=${line}
-  K8SSOURCE='k8s/_'${line}
-  DESTINATION=${line%/*}
-  echo "rm -rf ${CLEARPATH}"
-  echo "mv ${K8SSOURCE} ${DESTINATION}"
+  IFS=': ' read -a myarray <<< "${line}"
+  # echo "arraypos0: ${myarray[0]}"
+  # echo "arraypos1: ${myarray[1]}"
+  # echo "arraypos2: ${myarray[2]}"
+  if [ "${myarray[1]}" = "path" ]; then
+    TARGET="${myarray[2]}"
+    CLEARPATH="${TARGET}"
+    K8SSOURCE='k8s/_'${TARGET}
+    DESTINATION=${TARGET%/*}
+    rm -rf ${CLEARPATH}
+    mv -f ${K8SSOURCE} ${DESTINATION}
+  fi
 done <_data/overrides.yml
 
-#rm -rf k8s
+rm -rf k8s
+git add .
+git commit -m "Ran update-imported-docs.sh"
+echo "Docs imported! Run 'git status' to see proposed changes, 'git push' to upload them"
