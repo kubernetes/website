573e4631d619fe451f23d9af4deec2e03c5e53b8
Eric Tune <etune@google.com>
2016-11-28 13:33:21 -0800

    Use BSD-compatible sed arguments
    in update-generated-docs.sh
    so I can regenerate on my Mac.

--- update-imported-docs.sh
+++ update-imported-docs.sh
@@ -9,25 +9,25 @@ VERSION=1.5
 function process_api_ref_docs {
   # Replace html preview links by relative links to let k8s.io render them.
   local html_preview_prefix="https:\/\/htmlpreview.github.io\/?https:\/\/github.com\/kubernetes\/kubernetes\/blob\/"
-  find . -name '*.*' -type f -exec sed -i -e "s/${html_preview_prefix}HEAD//g" {} \;
-  find . -name '*.*' -type f -exec sed -i -e "s/${html_preview_prefix}release-$VERSION//g" {} \;
+  find . -name '*.*' -type f -exec sed -i "" -e "s/${html_preview_prefix}HEAD//g" {} \;
+  find . -name '*.*' -type f -exec sed -i "" -e "s/${html_preview_prefix}release-$VERSION//g" {} \;
 
   # Format html
-  find . -name '*.html' -type f -exec sed -i -e '/<style>/,/<\/style>/d' {} \;
-  find . -name '*.html' -type f -exec sed -i -e "s/http:\/\/kubernetes.io\/v$VERSION//g" {} \;
-  find . -name '*.html' -type f -exec sed -i -e '1 i\
+  find . -name '*.html' -type f -exec sed -i "" -e '/<style>/,/<\/style>/d' {} \;
+  find . -name '*.html' -type f -exec sed -i "" -e "s/http:\/\/kubernetes.io\/v$VERSION//g" {} \;
+  find . -name '*.html' -type f -exec sed -i "" -e '1 i\
 ---' {} \;
-  find . -name '*.html' -type f -exec sed -i -e '1 i\
+  find . -name '*.html' -type f -exec sed -i "" -e '1 i\
 ---' {} \;
 
   # Strip the munge comments
-  find . -name '*.md' -type f -exec sed -i -e '/<!-- BEGIN MUNGE: IS_VERSIONED -->/,/<!-- END MUNGE: IS_VERSIONED -->/d' {} \;
-  find . -name '*.md' -type f -exec sed -i -e '/<!-- BEGIN MUNGE: UNVERSIONED_WARNING -->/,/<!-- END MUNGE: UNVERSIONED_WARNING -->/d' {} \;
+  find . -name '*.md' -type f -exec sed -i "" -e '/<!-- BEGIN MUNGE: IS_VERSIONED -->/,/<!-- END MUNGE: IS_VERSIONED -->/d' {} \;
+  find . -name '*.md' -type f -exec sed -i "" -e '/<!-- BEGIN MUNGE: UNVERSIONED_WARNING -->/,/<!-- END MUNGE: UNVERSIONED_WARNING -->/d' {} \;
 
   # Add the expected headers to md files
-  find . -name '*.md' -type f -exec sed -i -e '1 i\
+  find . -name '*.md' -type f -exec sed -i "" -e '1 i\
 ---' {}  \;
-  find . -name '*.md' -type f -exec sed -i -e '1 i\
+  find . -name '*.md' -type f -exec sed -i "" -e '1 i\
 ---' {}  \;
 }
 
@@ -72,11 +72,11 @@ done <_data/overrides.yml
 pushd .
 cd _includes/v$VERSION
   # These are included in other files, so strip the DOCTYPE
-   find . -name '*.html' -type f -exec sed -i -e "s/<!DOCTYPE html>//g" {} \;
+   find . -name '*.html' -type f -exec sed -i "" -e "s/<!DOCTYPE html>//g" {} \;
 
   # Format html
-  find . -name '*.html' -type f -exec sed -i -e '/<style>/,/<\/style>/d' {} \;
-  find . -name '*.html' -type f -exec sed -i -e "s/http:\/\/kubernetes.io\/v$VERSION//g" {} \;
+  find . -name '*.html' -type f -exec sed -i "" -e '/<style>/,/<\/style>/d' {} \;
+  find . -name '*.html' -type f -exec sed -i "" -e "s/http:\/\/kubernetes.io\/v$VERSION//g" {} \;
 popd
 
 pushd .
@@ -84,7 +84,7 @@ cd docs/api-reference
   process_api_ref_docs
 
   # Fix for bug in 1.3 release
-  find . -name '*.md' -type f -exec sed -i -e "s/vv1.3.0-beta.0/v1.3/g" {} \;
+  find . -name '*.md' -type f -exec sed -i "" -e "s/vv1.3.0-beta.0/v1.3/g" {} \;
 popd
 
 pushd .
@@ -93,26 +93,26 @@ cd docs/federation/api-reference
 
   # Update the links from federation/docs/api-reference to
   # docs/federation/api-reference
-  find . -name '*.*' -type f -exec sed -i -e "s/federation\/docs\/api-reference/docs\/federation\/api-reference/g" {} \;
+  find . -name '*.*' -type f -exec sed -i "" -e "s/federation\/docs\/api-reference/docs\/federation\/api-reference/g" {} \;
 popd
 
 pushd .
 cd docs/user-guide/kubectl
   # Strip the munge comments
-  find . -name '*.md' -type f -exec sed -i -e '/<!-- BEGIN MUNGE: IS_VERSIONED -->/,/<!-- END MUNGE: IS_VERSIONED -->/d' {} \;
-  find . -name '*.md' -type f -exec sed -i -e '/<!-- BEGIN MUNGE: UNVERSIONED_WARNING -->/,/<!-- END MUNGE: UNVERSIONED_WARNING -->/d' {} \;
+  find . -name '*.md' -type f -exec sed -i "" -e '/<!-- BEGIN MUNGE: IS_VERSIONED -->/,/<!-- END MUNGE: IS_VERSIONED -->/d' {} \;
+  find . -name '*.md' -type f -exec sed -i "" -e '/<!-- BEGIN MUNGE: UNVERSIONED_WARNING -->/,/<!-- END MUNGE: UNVERSIONED_WARNING -->/d' {} \;
 
   # Rename kubectl.md to index.md
   mv kubectl.md index.md
   # Strip the "See Also" links. 
   # These links in raw .md files are relative to current file location, but the website see them as relative to current url instead, and will return 404.
-  find . -name 'kubectl*.md' -type f -exec sed -i -e '/### SEE ALSO/d' {} \; 
-  find . -name 'kubectl*.md' -type f -exec sed -i -e '/\* \[kubectl/d' {} \; 
+  find . -name 'kubectl*.md' -type f -exec sed -i "" -e '/### SEE ALSO/d' {} \; 
+  find . -name 'kubectl*.md' -type f -exec sed -i "" -e '/\* \[kubectl/d' {} \; 
 
 # Add the expected headers to md files
-  find . -name '*.md' -type f -exec sed -i -e '1 i\
+  find . -name '*.md' -type f -exec sed -i "" -e '1 i\
 ---' {} \;
-  find . -name '*.md' -type f -exec sed -i -e '1 i\
+  find . -name '*.md' -type f -exec sed -i "" -e '1 i\
 ---' {} \;
 popd
 
@@ -122,11 +122,11 @@ BINARIES="federation-apiserver.md federation-controller-manager.md kube-apiserve
 pushd .
 cd docs/admin
   for bin in $BINARIES; do
-  sed -i -e '/<!-- BEGIN MUNGE: IS_VERSIONED -->/,/<!-- END MUNGE: IS_VERSIONED -->/d' $bin
-  sed -i -e '/<!-- BEGIN MUNGE: UNVERSIONED_WARNING -->/,/<!-- END MUNGE: UNVERSIONED_WARNING -->/d' $bin
-  sed -i -e '1 i\
+  sed -i "" -e '/<!-- BEGIN MUNGE: IS_VERSIONED -->/,/<!-- END MUNGE: IS_VERSIONED -->/d' $bin
+  sed -i "" -e '/<!-- BEGIN MUNGE: UNVERSIONED_WARNING -->/,/<!-- END MUNGE: UNVERSIONED_WARNING -->/d' $bin
+  sed -i "" -e '1 i\
 ---' $bin
-  sed -i -e '1 i\
+  sed -i "" -e '1 i\
 ---' $bin
   done
 popd
