57595def61731e095688fbf5f80c17e18537e23a
Anthony Yeh <enisoc@google.com>
2016-12-06 15:39:58 -0800

    examples_test: Ignore PetSet object.
    It cannot be decoded anymore, but the file is kept for legacy users.

--- test/examples_test.go
+++ test/examples_test.go
@@ -264,7 +264,7 @@ func TestExampleObjectSchemas(t *testing.T) {
 			"nginx-probe-deployment":     {&extensions.Deployment{}},
 			"nginx-secure-app":           {&api.Service{}, &extensions.Deployment{}},
 			"nginx-svc":                  {&api.Service{}},
-			"petset":                     {&api.Service{}, &apps.StatefulSet{}},
+			"petset":                     {&api.Service{}, nil},
 			"pod":                        {&api.Pod{}},
 			"pod-w-message":              {&api.Pod{}},
 			"redis-deployment":           {&extensions.Deployment{}},
