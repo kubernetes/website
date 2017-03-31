de8428ad6d41dc1999ffcf2e434b9c3434f717f7
Andrew Chen <chenopis@google.com>
2017-03-31 01:27:49 -0700

    fix regexp

--- js/redirects.js
+++ js/redirects.js
@@ -3,13 +3,13 @@ $( document ).ready(function() {
     var fwdDirs = ["examples/","cluster/","docs/devel","docs/design"];
     var apiv1 = [{
         "from":"/docs/api-reference/v1/definitions",
-        "pattern":"#_v1_(\w+)",
+        "pattern":'#_v1_([A-Za-z0-9_]+)',
         "to":"/docs/api-reference/v1.6",
         "postfix":"-v1-core"
     },
     {
         "from":"/docs/user-guide/kubectl/kubectl_",
-        "pattern":"kubectl_(\w+)",
+        "pattern":'kubectl_([A-Za-z0-9_]+)',
         "to":"/docs/user-guide/kubectl/v1.6",
         "postfix":""
     }];
