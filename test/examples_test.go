49ec563bf5db4ec9950daa8de88ae8710d17af80
Chen Hong <chenhong@ghostcloud.cn>
2018-04-03 17:16:15 +0800

    fix mistake

--- test/examples_test.go
+++ test/examples_test.go
@@ -428,7 +428,7 @@ func TestExampleObjectSchemas(t *testing.T) {
 			"nginx-dep":             {&extensions.Deployment{}},
 			"shell-demo":            {&api.Pod{}},
 			"node-problem-detector":        {&extensions.DaemonSet{}},
-			"node-problem-detector-configmap":        {&api.ConfigMap{}},
+			"node-problem-detector-configmap":        {&extensions.DaemonSet{}},
 			"termination":           {&api.Pod{}},
 		},
 		// TODO: decide whether federation examples should be added
