fc980aa24744f04a3e34217675b2c59367919ec4
Claudia J.Kang <claudia.with.jh@gmail.com>
2018-06-01 21:06:06 +0900

    Update due to outdated source.

--- content/en/docs/tutorials/server.js
+++ content/en/docs/tutorials/server.js
@@ -1,9 +1,19 @@
 var http = require('http');
-
+var requests=0;
+var podname= process.env.HOSTNAME;
+var startTime;
+var host;
 var handleRequest = function(request, response) {
-  console.log('Received request for URL: ' + request.url);
+  response.setHeader('Content-Type', 'text/plain');
   response.writeHead(200);
-  response.end('Hello World!');
-};
+  response.write("Hello Kubernetes bootcamp! | Running on: ");
+  response.write(host);
+  response.end(" | v=1\n");
+  console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());
+}
 var www = http.createServer(handleRequest);
-www.listen(8080);
+www.listen(8080,function () {
+    startTime = new Date();;
+    host = process.env.HOSTNAME;
+    console.log ("Kubernetes Bootcamp App Started At:",startTime, "| Running On: " ,host, "\n" );
+});
