8846b6e7b67e7007ec8f3c312d1c0d4d7a820715
Qiming <tengqim@cn.ibm.com>
2018-07-03 04:06:18 +0800

    Consolidate YAML files [part-5] (#9258)
    * Consolidate YAML files [part-5]
    
    This PR relocates the yaml files used in stateless application
    guestbook. This PR also fixes the list numbering and code block
    problems in the page.
    
    Note that neither code blocks nor note callouts live happily inside a
    numbered list, so this PR moves them out of such lists.
    
    * Update examples_test.go

--- test/examples_test.go
+++ test/examples_test.go
@@ -463,6 +463,18 @@ func TestExampleObjectSchemas(t *testing.T) {
 			"nginx-with-request":    {&extensions.Deployment{}},
 			"shell-demo":            {&api.Pod{}},
 		},
+		"examples/application/guestbook": {
+			"frontend-deployment":     {&extensions.Deployment{}},
+			"frontend-service":        {&api.Service{}},
+			"redis-master-deployment": {&extensions.Deployment{}},
+			"redis-master-service":    {&api.Service{}},
+			"redis-slave-deployment":  {&extensions.Deployment{}},
+			"redis-slave-service":     {&api.Service{}},
+		},
+		"docs/tasks/run-application": {
+			"deployment-patch-demo": {&extensions.Deployment{}},
+			"hpa-php-apache":        {&autoscaling.HorizontalPodAutoscaler{}},
+		},
 		"examples/debug": {
 			"counter-pod":                     {&api.Pod{}},
 			"event-exporter":                  {&api.ServiceAccount{}, &rbac.ClusterRoleBinding{}, &extensions.Deployment{}},
@@ -506,14 +518,6 @@ func TestExampleObjectSchemas(t *testing.T) {
 			"local-volumes":        {&api.PersistentVolume{}, &api.PersistentVolume{}},
 			"mysql-deployment":     {&api.Service{}, &api.PersistentVolumeClaim{}, &extensions.Deployment{}},
 			"wordpress-deployment": {&api.Service{}, &api.PersistentVolumeClaim{}, &extensions.Deployment{}},
-		},
-		"docs/tutorials/stateless-application/guestbook": {
-			"frontend-deployment":     {&extensions.Deployment{}},
-			"frontend-service":        {&api.Service{}},
-			"redis-master-deployment": {&extensions.Deployment{}},
-			"redis-master-service":    {&api.Service{}},
-			"redis-slave-deployment":  {&extensions.Deployment{}},
-			"redis-slave-service":     {&api.Service{}},
 		},
 	}
 
