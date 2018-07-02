b5d18e6434b10c2c63a78df807df91c687f3baaa
Qiming <tengqim@cn.ibm.com>
2018-07-03 04:37:19 +0800

    Consolidate YAML files [part-6] (#9261)
    * Consolidate YAML files [part-6]
    
    This PR relocates the YAML files used by the stateful application
    examples.
    
    * Update examples_test.go

--- test/examples_test.go
+++ test/examples_test.go
@@ -471,6 +471,21 @@ func TestExampleObjectSchemas(t *testing.T) {
 			"redis-slave-deployment":  {&extensions.Deployment{}},
 			"redis-slave-service":     {&api.Service{}},
 		},
+		"examples/application/cassandra": {
+			"cassandra-service":     {&api.Service{}},
+			"cassandra-statefulset": {&apps.StatefulSet{}, &storage.StorageClass{}},
+		},
+		"examples/application/web": {
+			"web":			{&api.Service{}, &apps.StatefulSet{}},
+			"web-parallel":	{&api.Service{}, &apps.StatefulSet{}},
+		},
+		"examples/application/wordpress": {
+			"mysql-deployment":     {&api.Service{}, &api.PersistentVolumeClaim{}, &extensions.Deployment{}},
+			"wordpress-deployment": {&api.Service{}, &api.PersistentVolumeClaim{}, &extensions.Deployment{}},
+		},
+		"examples/application/zookeeper": {
+			"zookeeper": {&api.Service{}, &api.Service{}, &policy.PodDisruptionBudget{}, &apps.StatefulSet{}},
+		},
 		"docs/tasks/run-application": {
 			"deployment-patch-demo": {&extensions.Deployment{}},
 			"hpa-php-apache":        {&autoscaling.HorizontalPodAutoscaler{}},
@@ -504,20 +519,6 @@ func TestExampleObjectSchemas(t *testing.T) {
 		"docs/concepts/overview/object-management-kubectl": {
 			"simple_deployment": {&extensions.Deployment{}},
 			"update_deployment": {&extensions.Deployment{}},
-		},
-		"docs/tutorials/stateful-application": {
-			"web":       {&api.Service{}, &apps.StatefulSet{}},
-			"webp":      {&api.Service{}, &apps.StatefulSet{}},
-			"zookeeper": {&api.Service{}, &api.Service{}, &policy.PodDisruptionBudget{}, &apps.StatefulSet{}},
-		},
-		"docs/tutorials/stateful-application/cassandra": {
-			"cassandra-service":     {&api.Service{}},
-			"cassandra-statefulset": {&apps.StatefulSet{}, &storage.StorageClass{}},
-		},
-		"docs/tutorials/stateful-application/mysql-wordpress-persistent-volume": {
-			"local-volumes":        {&api.PersistentVolume{}, &api.PersistentVolume{}},
-			"mysql-deployment":     {&api.Service{}, &api.PersistentVolumeClaim{}, &extensions.Deployment{}},
-			"wordpress-deployment": {&api.Service{}, &api.PersistentVolumeClaim{}, &extensions.Deployment{}},
 		},
 	}
 
