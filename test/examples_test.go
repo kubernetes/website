d0d4f7cced400f66fe719ddcc861d8307aa9de38
zacharysarah <zach@corleissen.com>
2018-07-11 13:04:59 -0700

    Updating 1.9 navigation
    Update 1.10 path
    
    Trying to debug Travis CI failure
    
    Update test case from #9423
    
    Updating 1.9 navigation
    
    Update 1.10 path
    
    Trying to debug Travis CI failure
    
    Update from #9423
    
    1.9, not 1.8

--- test/examples_test.go
+++ test/examples_test.go
@@ -42,8 +42,8 @@ import (
 	autoscaling_validation "k8s.io/kubernetes/pkg/apis/autoscaling/validation"
 	"k8s.io/kubernetes/pkg/apis/batch"
 	batch_validation "k8s.io/kubernetes/pkg/apis/batch/validation"
-	api "k8s.io/kubernetes/pkg/apis/core"
-	"k8s.io/kubernetes/pkg/apis/core/validation"
+	api "k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/validation"
 	"k8s.io/kubernetes/pkg/apis/extensions"
 	ext_validation "k8s.io/kubernetes/pkg/apis/extensions/validation"
 	"k8s.io/kubernetes/pkg/apis/policy"
@@ -56,7 +56,7 @@ import (
 	storage_validation "k8s.io/kubernetes/pkg/apis/storage/validation"
 	"k8s.io/kubernetes/pkg/capabilities"
 	"k8s.io/kubernetes/pkg/registry/batch/job"
-	schedulerapilatest "k8s.io/kubernetes/pkg/scheduler/api/latest"
+	schedulerapilatest "k8s.io/kubernetes/plugin/pkg/scheduler/api/latest"
 )
 
 func validateObject(obj runtime.Object) (errors field.ErrorList) {
