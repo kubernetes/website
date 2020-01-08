743e6d468ac486e32bf1de80d08de82580d74b76
Supriya Sirbi <supriya.sirbi@gmail.com>
2020-01-08 15:30:09 +0530

    Issue #18498 Convert URL Request and Hello World to Korean

--- content/ko/examples/minikube/server.js
+++ content/ko/examples/minikube/server.js
@@ -1,9 +1,9 @@
 var http = require('http');
 
 var handleRequest = function(request, response) {
-  console.log('Received request for URL: ' + request.url);
+  console.log('URL 요청을 받았습니다 ' + request.url);
   response.writeHead(200);
-  response.end('Hello World!');
+  response.end('안녕 세상');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
