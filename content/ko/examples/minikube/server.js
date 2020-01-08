048ace19c01bbb2b73bcc27fa318860a1553cd72
Supriya Sirbi <supriya.sirbi@gmail.com>
2020-01-08 16:10:17 +0530

    Update server.js

--- content/ko/examples/minikube/server.js
+++ content/ko/examples/minikube/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('URL 요청을 받았습니다 ' + request.url);
   response.writeHead(200);
-  response.end('안녕 세상');
+  response.end('Hello World!');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
