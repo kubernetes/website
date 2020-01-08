3e16b613ea3a125b90741620be56d6f0aba5ff95
Supriya Sirbi <supriya.sirbi@gmail.com>
2020-01-08 15:38:01 +0530

    Issue #18497 Update URL Request and Hello World to Japanese

--- content/ja/examples/minikube/server.js
+++ content/ja/examples/minikube/server.js
@@ -1,9 +1,9 @@
 var http = require('http');
 
 var handleRequest = function(request, response) {
-  console.log('Received request for URL: ' + request.url);
+  console.log('URLのリクエストを受信しました ' + request.url);
   response.writeHead(200);
-  response.end('Hello World!');
+  response.end('こんにちは世界');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
