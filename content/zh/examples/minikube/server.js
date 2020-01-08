49aae5b7e39adab815d43dc6f7858e096d911676
Supriya Sirbi <supriya.sirbi@gmail.com>
2020-01-08 15:48:45 +0530

    Issue #18496 Convert URL Request and Hello World to Chinese

--- content/zh/examples/minikube/server.js
+++ content/zh/examples/minikube/server.js
@@ -1,9 +1,9 @@
 var http = require('http');
 
 var handleRequest = function(request, response) {
-  console.log('Received request for URL: ' + request.url);
+  console.log('收到网址请求 ' + request.url);
   response.writeHead(200);
-  response.end('Hello World!');
+  response.end('你好，世界');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
