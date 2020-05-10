c8f442c758b1946729b14ff4c2e80ff948c35e6d
Aidan Ranney <aidan.s.ranney@gmail.com>
2020-05-10 11:59:45 -0700

    Fixes to enable search.

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,12 +1,9 @@
 /*
 Copyright 2018 Google LLC
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     https://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,30 +11,26 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-(function($) {
+(function ($) {
+  "use strict";
 
-    'use strict';
+  var Search = {
+    init: function () {
+      $(document).ready(function () {
+        $(document).on("keypress", ".td-search-input", function (e) {
+          if (e.keyCode !== 13) {
+            return;
+          }
 
-    var Search = {
-        init: function() {
-            $(document).ready(function() {
-               $(document).on('keypress', '.td-search-input', function(e) {
-                    if (e.keyCode !== 13) {
-                        return
-                    }
+          var query = $(this).val();
+          var searchPage = "{{ "docs/search/" | absURL }}?q=" + query;
+          document.location = searchPage;
 
-                    var query = $(this).val();
-                    var searchPage = "{{ "docs/search/" | absURL }}?q=" + query;
-                    document.location = searchPage;
+          return false;
+        });
+      });
+    },
+  };
 
-                    return false;
-                });
-
-            });
-        },
-    };
-
-    Search.init();
-
-
-}(jQuery));
+  Search.init();
+})(jQuery);
