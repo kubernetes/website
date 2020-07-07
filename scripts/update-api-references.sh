06a6fad8608706ea3c9ac7270bb74ccd2a586a46
Karen Bradshaw <kbhawkey@gmail.com>
2020-07-07 13:00:23 -0400

    clean up api ref snapshot _redirects

--- scripts/update-api-references.sh
+++ scripts/update-api-references.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Script to update the local copies of the 4 previous API reference files.
+# Script to update the four previous API reference files.
 # Set K8S_WEBSITE in your env to your docs website root.
-
 # Usage: ./update-api-references.sh 1.18
 
 if [ -n "$1" ] ; then
@@ -36,31 +35,102 @@ if [ -d "static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_5}/" ] 
     git rm -rf static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_5}/*
     echo "Removing API reference directory: static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_5}"
 else
-    echo "Failed to remove last previous API reference."
+    echo "Could not remove last previous API reference."
     echo "Directory does not exist: static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_5}"
 fi
 
 i=1
 echo "Copying last four previous API references files"
+
+# Update previous index.html files until all API reference index.html files use
+# the same paths to asset locations
+
 while [ $i -lt ${FIVE} ] ; do
     RELEASE_MINUS_NUM="${MAJOR_VERSION}.""$((${MINOR_VERSION} - ${i}))"
     echo "Copying release version: ${RELEASE_MINUS_NUM}"
-    git checkout upstream/release-${RELEASE_MINUS_NUM} static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html
-    git checkout upstream/release-${RELEASE_MINUS_NUM} static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/js/navData.js
 
-    # Verify that files are copied
+    # remove the existing index.html
     if [ -f "static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html" ] ; then
-        echo "Copied API reference files: static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html"
+        rm static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html
+    fi
+
+    git checkout upstream/release-${RELEASE_MINUS_NUM} static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html
+
+    if [ -f "static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html" ] ; then
+        echo "Copied API reference file: static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html"
+
+        # There are four versions that use a previous version of the stylesheet.
+        # Also, four versions have paths to assets that are different from the assets in site's static directory.
+        # Rewrite paths to assets for versions 1.14, 1.15, 1.16, 1.17
+        # Use previous style_apiref_prev.css instead of style_apiref.css
+        MINUS_NUM=$((${MINOR_VERSION} - ${i}))
+        if [ ${MINUS_NUM} -ge 14 ] || [ ${MINUS_NUM} -lt 17 ] ; then
+            sed -i -e "s/css\/stylesheet.css/\/css\/style_apiref_prev.css/g" static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html
+            sed -i -e "s/css\/bootstrap.min.css/\/css\/bootstrap-4.3.1.min.css/g" static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html
+            sed -i -e "s/css\/font-awesome.min.css/\/css\/fontawesome-4.7.0.min.css/g" static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html
+
+            sed -i -e "s/^<SCRIPT[[:space:]]src=\"\/js\/jquery-3.2.1.min.js\"/<SCRIPT src=\"\/js\/jquery-3.3.1.min.js\"/g" static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html
+            sed -i -e "s/^<SCRIPT[[:space:]]src=\"jquery.scrollTo.min.js\"/<SCRIPT src=\"\/js\/jquery.scrollTo-2.1.2.min.js\"/g" static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html
+            sed -i -e "s/^<SCRIPT[[:space:]]src=\"navData.js\"/<SCRIPT src=\"js\/navData.js\"/g" static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html
+            sed -i -e "s/^<SCRIPT[[:space:]]src=\"scroll.js\"/<SCRIPT src=\"\/js\/scroll-apiref.js\"/g" static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html
+        fi
+
+        # version 1.17 asset paths are slightly different
+        if [ ${MINUS_NUM} -eq 17 ] ; then
+            sed -i -e "s/css\/stylesheet.css/\/css\/style_apiref_prev.css/g" static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html
+            sed -i -e "s/css\/bootstrap.min.css/\/css\/bootstrap-4.3.1.min.css/g" static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html
+            sed -i -e "s/css\/font-awesome.min.css/\/css\/fontawesome-4.7.0.min.css/g" static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html
+
+            sed -i -e "s/^<SCRIPT[[:space:]]src=\"\/js\/jquery-3.2.1.min.js\"/<SCRIPT src=\"\/js\/jquery-3.3.1.min.js\"/g" static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html
+            sed -i -e "s/^<SCRIPT[[:space:]]src=\"js\/jquery.scrollTo.min.js\"/<SCRIPT src=\"\/js\/jquery.scrollTo-2.1.2.min.js\"/g" static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html
+            sed -i -e "s/^<SCRIPT[[:space:]]src=\"js\/scroll.js\"/<SCRIPT src=\"\/js\/scroll-apiref.js\"/g" static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html
+        fi
+# 1.14- 1.16
+#<SCRIPT src="/js/jquery-3.2.1.min.js"></SCRIPT>
+#<SCRIPT src="jquery.scrollTo.min.js"></SCRIPT>
+#<SCRIPT src="/js/bootstrap-4.3.1.min.js"></SCRIPT>
+#<SCRIPT src="navData.js"></SCRIPT>
+#<SCRIPT src="scroll.js"></SCRIPT>
+
+# 1.17
+#<SCRIPT src="/js/jquery-3.2.1.min.js"></SCRIPT>
+#<SCRIPT src="js/jquery.scrollTo.min.js"></SCRIPT>
+#<SCRIPT src="/js/bootstrap-4.3.1.min.js"></SCRIPT>
+#<SCRIPT src="js/navData.js"></SCRIPT>
+#<SCRIPT src="js/scroll.js"></SCRIPT>
+
+# 1.18
+#<SCRIPT src="/js/jquery-3.3.1.min.js"></SCRIPT>
+#<SCRIPT src="/js/jquery.scrollTo-2.1.2.min.js"></SCRIPT>
+#<SCRIPT src="/js/bootstrap-4.3.1.min.js"></SCRIPT>
+#<SCRIPT src="js/navData.js"></SCRIPT>
+#<SCRIPT src="/js/scroll-apiref.js"></SCRIPT>
+
         git add static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html
     else
         echo "Failed to copy static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/index.html"
     fi
 
-    if [ -f "static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/js/navData.js" ] ; then
-        echo "Copied API reference files: static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/js/navData.js"
+    # Try to copy js/navData.js; For some versions, navData.js is at the root directory
+    git checkout upstream/release-${RELEASE_MINUS_NUM} static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/js/navData.js
+
+    # This is a temporary check, until the paths to assets are synced up.
+    if [ -f "static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/js/navData.js" ]; then
+        echo "Copied API reference file: static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/js/navData.js"
         git add static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/js/navData.js
     else
-        echo "Failed to copy static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/js/navData.js"
+        echo "Trying to check out static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/navData.js"
+        git checkout upstream/release-${RELEASE_MINUS_NUM} static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/navData.js
+
+        if [ -f "static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/navData.js" ] ; then
+            mkdir static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/js
+            mv static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/navData.js static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/js/navData.js
+            echo "Copied API reference files: static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/js/navData.js"
+            git add static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/js/navData.js
+            git rm static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/navData.js
+        else
+            echo "Failed to copy static/docs/reference/generated/kubernetes-api/v${RELEASE_MINUS_NUM}/navData.js"
+        fi
     fi
 
     ((i++))
