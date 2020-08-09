34d805df7280f40c93a3c832f117caa7ec964c76
TAKAHASHI Shuuji <shuuji3@gmail.com>
2020-08-09 12:14:40 +0900

    Fill the search input form with the current search keywords.

--- assets/js/search.js
+++ assets/js/search.js
@@ -17,6 +17,15 @@ limitations under the License.
   var Search = {
     init: function () {
       $(document).ready(function () {
+        // Fill the search input form with the current search keywords
+        const searchKeywords = new URLSearchParams(location.search).get('q');
+        if (searchKeywords !== null && searchKeywords !== '') {
+          const searchInput = document.querySelector('.td-search-input');
+          searchInput.focus();
+          searchInput.value = searchKeywords;
+        }
+
+        // Set a keydown event
         $(document).on("keypress", ".td-search-input", function (e) {
           if (e.keyCode !== 13) {
             return;
