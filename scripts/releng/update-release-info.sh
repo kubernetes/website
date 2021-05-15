f00c145ead1c2e146885048b4c51edba747b640d
Stephen Augustus <foo@auggie.dev>
2021-05-15 15:49:41 -0400

    releases: Canonicalize patch release information on k/website
    Signed-off-by: Stephen Augustus <foo@auggie.dev>

--- scripts/releng/update-release-info.sh
+++ scripts/releng/update-release-info.sh
@@ -1,24 +1,5 @@
 #!/usr/bin/env bash
 
-# patch-releases.md
-####################################
-cat << EOF > content/en/releases/patch-releases.md
----
-title: Patch Releases
-type: docs
-auto_generated: true
----
-<!-- THIS CONTENT IS AUTO-GENERATED via ./scripts/update-release-info.sh in k/website -->
-
-{{< warning >}}
-This content is auto-generated and links may not function. The source of the document is located [here](https://github.com/kubernetes/sig-release/blob/master/releases/patch-releases.md).
-{{< /warning >}}
-
-EOF
-
-curl --retry 3 https://raw.githubusercontent.com/kubernetes/sig-release/master/releases/patch-releases.md >> content/en/releases/patch-releases.md
-
-
 # release.md
 ####################################
 cat << EOF > content/en/releases/release.md
@@ -36,8 +17,3 @@ This content is auto-generated and links may not function. The source of the doc
 EOF
 
 curl --retry 3 https://raw.githubusercontent.com/kubernetes/community/master/contributors/devel/sig-release/release.md >> content/en/releases/release.md
-
-
-# schedule.yaml
-####################################
-curl --retry 3 https://raw.githubusercontent.com/kubernetes/sig-release/master/releases/schedule.yaml > data/releases/schedule.yaml
