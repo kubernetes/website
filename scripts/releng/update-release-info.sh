2021adac4a9287d26ee45cc6e6ab6d63cd26e5d5
Stephen Augustus <foo@auggie.dev>
2021-05-19 17:30:51 -0400

    releases: Update script reference and front matter for release.md
    Signed-off-by: Stephen Augustus <foo@auggie.dev>

--- scripts/releng/update-release-info.sh
+++ scripts/releng/update-release-info.sh
@@ -4,11 +4,11 @@
 ####################################
 cat << EOF > content/en/releases/release.md
 ---
-title: The Release Cycle
+title: Kubernetes Release Cycle
 type: docs
 auto_generated: true
 ---
-<!-- THIS CONTENT IS AUTO-GENERATED via ./scripts/update-release-info.sh in k/website -->
+<!-- THIS CONTENT IS AUTO-GENERATED via ./scripts/releng/update-release-info.sh in k/website -->
 
 {{< warning >}}
 This content is auto-generated and links may not function. The source of the document is located [here](https://github.com/kubernetes/community/blob/master/contributors/devel/sig-release/release.md).
